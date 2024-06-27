63a7e15cc21d666ced02cc6f0d1651fbc259583c
Marko Zecevic <markzecevic99@gmail.com>
2024-06-27 04:29:17 +0200

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ColorPicker from './ColorPicker.jsx'
+
+import './App.css'
+
+function App() {
+ 
+return(
+  <ColorPicker />
+
+)
+
+}
+
+export default App
